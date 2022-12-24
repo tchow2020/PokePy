fc08151e3fe7907b1c12ee874efe1306580a49a4
Richards0n <richardson.douglas.31@gmail.com>
2022-12-23 23:31:40 -0300

    refactor: :recycle: Refatorado toda a aplicação principal
    Foi adicionado um sistema de renderização de templates, rotas, sistema de busca através de um formulário e afins...

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 from models.pokemon import Pokemon
-from flask import Flask
+from models.pokemonf import PokemonF
+from flask import Flask, render_template, request, url_for, flash, redirect
 from flask_restful import Api
 
 app = Flask(__name__)
-api = Api(app)
+# api = Api(app)
 
-api.add_resource(Pokemon.GetPokemon, ('/Pokemon/find/<string:name>'))
-api.add_resource(Pokemon.Homologação, ('/Pokemon/Homologação'))
+# api.add_resource(Pokemon.GetPokemon, ('/Pokemon/find/<string:name>'))
+# api.add_resource(Pokemon.Homologação, ('/Pokemon/Homologação'))
+
+
+@app.route('/', methods = ['GET', 'POST'])
+def index():
+    if request.method == 'GET':
+        return render_template('index.html', listPokemon = PokemonF.getAllPokemon(), listAll = True)
+    if request.method == 'POST':
+        return render_template('index.html', listPokemon = PokemonF.getPokemon(request.form['nome']))
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ models/pokemonf.py
@@ -0,0 +1,75 @@
+import requests
+
+class PokemonF(): 
+    def getPokemon(name):
+        name = name.lower()
+        result = requests.get('https://pokeapi.co/api/v2/pokemon/?&limit=1154')
+        dados = result.json()
+        listarPokemon = dados["results"]
+        for pokemon in listarPokemon:
+            if pokemon["name"] == name:
+                urlPokemon = pokemon["url"]
+                result = requests.get(urlPokemon)
+                dados = result.json()
+                
+                ability = []
+                for i in dados["abilities"]:
+                    ability.append(i["ability"]["name"])
+
+                type = []
+                for i in dados["types"]:
+                    type.append(i["type"]["name"])
+
+                moves = []
+                for i in dados["moves"]:
+                    moves.append(i["move"]["name"])
+
+                jsonReturn = {
+                    "image": dados["sprites"]["front_default"],
+                    "nome": dados["forms"][0]["name"],
+                    "ability": ability,
+                    "types": type,
+                    "moves":moves
+                }
+
+                return jsonReturn
+                
+        return {
+            'error': True,
+            'msg': 'Não identifiquei este pokemon'
+        }
+    
+    def getAllPokemon():
+        result = requests.get('https://pokeapi.co/api/v2/pokemon/?&limit=10')
+        dados = result.json()
+        listarPokemon = dados["results"]
+
+        allPokemon = []
+
+        for pokemon in listarPokemon:
+            urlPokemon = pokemon["url"]
+            result = requests.get(urlPokemon)
+            dados = result.json()
+            
+            ability = []
+            for i in dados["abilities"]:
+                ability.append(i["ability"]["name"])
+
+            type = []
+            for i in dados["types"]:
+                type.append(i["type"]["name"])
+
+            moves = []
+            for i in dados["moves"]:
+                moves.append(i["move"]["name"])
+
+            jsonReturn = {
+                "image": dados["sprites"]["front_default"],
+                "nome": dados["forms"][0]["name"],
+                "ability": ability,
+                "types": type,
+                "moves":moves
+            }
+            allPokemon.append(jsonReturn)
+
+        return allPokemon
